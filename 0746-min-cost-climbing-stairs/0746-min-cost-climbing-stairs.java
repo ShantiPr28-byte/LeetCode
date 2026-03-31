2ecbdd39c3f51199840485228cee61fea0a47954
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-03-31 23:43:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,23 +1,10 @@
 class Solution {
-    int[] dp;
     public int minCostClimbingStairs(int[] cost) {
-        dp = new int[cost.length];
-        Arrays.fill(dp, -1);
-
-        int case1 = solve(0, cost);
-        int case2= solve(1, cost);
-
-        return Math.min(case1, case2);
+        return Math.min(minCost(0, cost), minCost(1, cost));
     }
 
-    private int solve(int i, int[] cost) {
+    private int minCost(int i, int[] cost) {
         if(i >= cost.length) return 0;
-
-        if(dp[i] != -1) return dp[i];
-
-        int oneStep = cost[i] + solve(i+1, cost);
-        int twoStep = cost[i] + solve(i+2, cost);
-
-        return dp[i] = Math.min(oneStep, twoStep);
+        return cost[i] + Math.min(minCost(i+1, cost), minCost(i+2, cost));
     }
 }

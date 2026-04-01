1af2cc3d517217afbebadf686aeaeb85e44ac832
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-04-01 09:23:43 +0530

    Time: 0 ms (100%), Space: 42.8 MB (46.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,19 +1,35 @@
 class Solution {
-    int[] dp;
+    // int[] dp;
     public int rob(int[] nums) {
-        dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return solve(0, nums);
+        // dp = new int[nums.length];
+        // Arrays.fill(dp, -1);
+        // return solve(0, nums);
+
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        if(n == 2) return(Math.max(nums[0], nums[1]));
+
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        int maxAmount = Integer.MIN_VALUE;
+        for(int i = 2; i < n; i++) {
+            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+            maxAmount = Math.max(maxAmount, dp[i]);
+        }
+
+        return maxAmount;
     }
 
-    private int solve(int i, int[] nums) {
-        if(i >= nums.length) return 0;
+    // private int solve(int i, int[] nums) {
+    //     if(i >= nums.length) return 0;
 
-        if(dp[i] != -1) return dp[i];
+    //     if(dp[i] != -1) return dp[i];
 
-        int take = nums[i] + solve(i+2, nums);
-        int skip = solve(i+1, nums);
+    //     int take = nums[i] + solve(i+2, nums);
+    //     int skip = solve(i+1, nums);
 
-        return dp[i] = Math.max(take, skip);
-    }
+    //     return dp[i] = Math.max(take, skip);
+    // }
 }

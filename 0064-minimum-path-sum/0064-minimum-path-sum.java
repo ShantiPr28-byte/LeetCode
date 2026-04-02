834571a65d6144d07132339413b202744f5872df
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-04-02 11:17:56 +0530

    Time: 1 ms (99.8%), Space: 49.5 MB (97.86%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,19 +3,23 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        return solve(m-1, n-1, grid);
+        Integer[][] dp = new Integer[m][n];
+
+        return solve(m-1, n-1, grid, dp);
     }
 
-    private int solve(int i, int j, int[][] grid) {
+    private int solve(int i, int j, int[][] grid, Integer[][] dp) {
         //base case 
         if(i == 0 && j == 0) return grid[0][0];
 
         //base case
         if(i < 0 || j < 0) return Integer.MAX_VALUE;
 
-        int up = solve(i-1, j, grid);
-        int left = solve(i, j-1, grid);
+        if(dp[i][j] != null) return dp[i][j];
 
-        return grid[i][j] + Math.min(up, left);
+        int up = solve(i-1, j, grid, dp);
+        int left = solve(i, j-1, grid, dp);
+
+        return dp[i][j] = grid[i][j] + Math.min(up, left);
     }
 }

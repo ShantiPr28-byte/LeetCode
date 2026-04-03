1fd4eb7cf646178bf2097eb2089d8ed71ed0c9b6
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-04-03 11:56:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -7,32 +7,17 @@ class Solution {
         if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
 
         int[][] dp = new int[m][n];
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                dp[i][j] = 0;
+        dp[m-1][n-1] = 1;
+        for(int i = m-1; i >= 0; i--) {
+            for(int j = n-1; j >= 0; j--) {
+                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else {
+                    if(i+1 < m) dp[i][j] += dp[i+1][j];
+                    if(j+1 < n) dp[i][j] += dp[i][j+1];
+                }
             }
         }
 
-        for(int j = 0; j < n; j++) {
-            if(obstacleGrid[0][j] == 1) {
-                break;
-            }
-            dp[0][j] = 1;
-        }
-        for(int i = 1; i < m; i++) {
-            if(obstacleGrid[i][0] == 1) {
-                break;
-            }
-            dp[i][0] = 1;
-        }
-
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                if(obstacleGrid[i][j] == 1) continue;
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-
-        return dp[m-1][n-1];
+        return dp[0][0];
     }
 }

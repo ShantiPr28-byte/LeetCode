17a2bc0eb8c6552342d23b1511308a27b4607184
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-04-19 12:29:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -12,21 +12,23 @@ class Solution {
 
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{sr, sc});
+        image[sr][sc] = color;
 
         while(!q.isEmpty()) {
             int[] curr = q.poll();
             int r = curr[0];
             int c = curr[1];
 
-            image[r][c] = color;
-
             for(int k = 0; k < 4; k++) {
                 int R = r + dr[k];
                 int C = c + dc[k];
 
-                if(R < 0 || C < 0 || R >= m || C >= n || image[R][C] != src) continue;
+                if(R < 0 || C < 0 || R >= m || C >= n) continue;
 
-                q.offer(new int[]{R, C});
+                if(image[R][C] == src) {
+                    image[R][C] = color;
+                    q.offer(new int[]{R, C});
+                }
             }
         }
 

8ae531e196717c0528ef3c59bf147a39c7f5e1cf
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-04-26 18:05:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,28 +1,23 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
+        
         List<List<Integer>> result = new ArrayList<>();
-
-        solve(0, nums, new ArrayList<>(), result);
-
+        backtrack(0, nums, new ArrayList<>(), result);
+        
         return result;
     }
 
-    private void solve(int index, int[] nums, List<Integer> subset, List<List<Integer>> result) {
-        if(index == nums.length) {
-            result.add(new ArrayList<>(subset));
-            return;
+    private void backtrack(int start, int[] nums, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+
+        for (int i = start; i < nums.length; i++) {
+
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            current.add(nums[i]);                // choose
+            backtrack(i + 1, nums, current, result); // explore
+            current.remove(current.size() - 1);  // un-choose (backtrack)
         }
-
-        subset.add(nums[index]);
-        solve(index + 1, nums, subset, result);
-
-        subset.remove(subset.size() - 1);
-
-        while(index + 1 < nums.length && nums[index] == nums[index+1]) {
-            index++;
-        }
-
-        solve(index + 1, nums, subset, result);
     }
 }

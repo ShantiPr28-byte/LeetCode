e06df5562ce90af087f083ac864bae7af3f1ab2d
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-04-26 16:13:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,26 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> powerSet = new ArrayList<>();
+        powerSet.add(new ArrayList<>());
 
-        solve(0, new ArrayList<>(), powerSet, nums);
+        for(int num : nums) {
+            int size = powerSet.size();
+
+            for(int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(powerSet.get(i));
+                subset.add(num);
+                powerSet.add(subset);
+            }
+        }
 
         return powerSet;
-    }
-
-    private void solve(int index, List<Integer> subset, List<List<Integer>> powerSet, int[] nums) {
-        if(index == nums.length) {
-            powerSet.add(new ArrayList<>(subset));
-            return;
-        } 
-
-        //Add element
-        subset.add(nums[index]);
-        solve(index + 1, subset, powerSet, nums);
-
-        //Backtrack
-        subset.remove(subset.size() - 1);
-
-        //Skip element
-        solve(index + 1, subset, powerSet, nums);
     }
 }

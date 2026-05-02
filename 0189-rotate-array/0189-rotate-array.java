c7f6316a89f9d2b706ee5a5eb548dff075db9eba
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-05-02 18:02:25 +0530

    Time: 2 ms (22.53%), Space: 61.5 MB (29.43%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,14 +1,33 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        // Juggling algorithm
         int n = nums.length;
+        k = k % n;
+        int cycles = gcd(n, k);
 
-        for(int i = 0; i < k; i++) {
-            int last = nums[n-1];
+        for(int i = 0; i < cycles; i++) {
+            int currIdx = i;
+            int prevValue = nums[i];
 
-            for(int j = n-1; j > 0; j--) {
-                nums[j] = nums[j - 1];
+            while(true) {
+                int nextIdx = (currIdx + k) % n;
+                int temp = nums[nextIdx];
+                nums[nextIdx] = prevValue;
+
+                prevValue = temp;
+                currIdx = nextIdx;
+
+                if (currIdx == i) break;
             }
-            nums[0] = last;
         }
+    }
+
+    private int gcd(int a, int b) {
+        while(b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }

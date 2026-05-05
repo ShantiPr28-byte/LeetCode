a1d92832340620b7e40e0da6e93614f4aaf129d7
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-05-05 22:20:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,23 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(s.length() == 1) return true;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(Character.isLetterOrDigit(ch)) {
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
 
+        return checkPalindrome(sb);
+    }
+
+    private boolean checkPalindrome(StringBuilder sb) {
         int left = 0;
-        int right = s.length() - 1;
+        int right = sb.length() - 1;
 
         while(left < right) {
-            while(left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
-            }
-
-            while(left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            }
-
-            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+            if(sb.charAt(left) != sb.charAt(right)) {
                 return false;
             }
-
             left++;
             right--;
         }

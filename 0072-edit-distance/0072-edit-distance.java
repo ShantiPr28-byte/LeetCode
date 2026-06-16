0ae031f0da6e7555efdc38011d6e0a21295524c8
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-06-16 23:06:21 +0530

    Time: 2 ms (100%), Space: 47.5 MB (6%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,25 +1,34 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        Integer[][] dp = new Integer[word1.length()][word2.length()];
-        return solve(word1, word2, word1.length()-1, word2.length()-1, dp);
+        int len1 = word1.length();
+        int len2 = word2.length();
+
+        Integer[][] dp = new Integer[len1][len2];
+
+        return solve(len1 - 1, len2 - 1, word1, word2, dp);
     }
 
-    private int solve(String word1, String word2, int i, int j, Integer[][] dp) {
-        if(i < 0) return j + 1;
-        if(j < 0) return i + 1;
-
-        if(dp[i][j] != null) {
-            return dp[i][j];
+    private int solve(int n1, int n2, String word1, String word2, Integer[][] dp) {
+        if(n1 < 0) {
+            return n2 + 1;
+        }
+        if(n2 < 0) {
+            return n1 + 1;
         }
 
-        if(word1.charAt(i) == word2.charAt(j)) {
-            return dp[i][j] = solve(word1, word2, i-1, j-1, dp);
+        if(dp[n1][n2] != null) {
+            return dp[n1][n2];
         }
 
-        int insert = solve(word1, word2, i, j-1, dp);
-        int delete = solve(word1, word2, i-1, j, dp);
-        int replace = solve(word1, word2, i-1, j-1, dp);
+        int insert = 0, delete = 0, replace = 0;
+        if(word1.charAt(n1) == word2.charAt(n2)) {
+            return dp[n1][n2] = solve(n1 - 1, n2 - 1, word1, word2, dp);
+        } else {
+            insert = 1 + solve(n1, n2 - 1, word1, word2, dp);
+            delete = 1 + solve(n1 - 1, n2, word1, word2, dp);
+            replace = 1 + solve(n1 - 1, n2 - 1, word1, word2, dp);
+        }
 
-        return dp[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
-    }
+        return dp[n1][n2] = Math.min(Math.min(insert, delete), replace);
+    } 
 }

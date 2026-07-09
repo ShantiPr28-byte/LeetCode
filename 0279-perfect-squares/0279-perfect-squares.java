455713c69464a5c64c344e9cf052aafe0edf5b7f
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-07-09 14:06:41 +0530

    Time: 1 ms (99.24%), Space: 42.6 MB (96.38%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,16 +1,21 @@
 class Solution {
     public int numSquares(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        if(isSquare(n)) return 1;
 
-        dp[0] = 0;
-
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
-            }
+        for(int i = 1; i * i <= n; i++) {
+            int x = n - i * i;
+            if(isSquare(x)) return 2;
         }
 
-        return dp[n];
+        while(n % 4 == 0) n /= 4;
+
+        if(n % 8 == 7) return 4;
+
+        return 3;
+    }
+
+    private boolean isSquare(int n) {
+        int r = (int) Math.sqrt(n);
+        return r * r == n;
     }
 }

572d6e99dbbea31fb249c6070fe8cc1f70be2c81
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-07-14 23:14:35 +0530

    Time: 41 ms (89.38%), Space: 177.3 MB (88.38%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.java
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -1,13 +1,10 @@
 class Solution {
-    int[] dr = {-1, 1, 0, 0};
-    int[] dc = {0, 0, -1, 1};
-
     public int[][] highestPeak(int[][] isWater) {
         int m = isWater.length;
         int n = isWater[0].length;
 
         Queue<int[]> q = new LinkedList<>();
-        
+
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(isWater[i][j] == 1) {
@@ -19,6 +16,9 @@ class Solution {
             }
         }
 
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
         while(!q.isEmpty()) {
             int[] curr = q.poll();
             int r = curr[0];
@@ -29,6 +29,7 @@ class Solution {
                 int C = c + dc[i];
 
                 if(R < 0 || C < 0 || R >= m || C >= n || isWater[R][C] != -1) continue;
+
                 isWater[R][C] = isWater[r][c] + 1;
                 q.offer(new int[] {R, C});
             }

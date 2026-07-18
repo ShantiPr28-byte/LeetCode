fe872d423b2f16f067b6af8ba7c3109bb0c02263
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-07-18 18:10:18 +0530

    Time: 5 ms (99.82%), Space: 48.6 MB (97.54%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,11 +1,22 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
+        int[] freq = new int[20001];
+        
+        for(int i = 0; i < nums.length; i++) {
+            freq[nums[i] + 10000]++;
+        }
 
         int ans = 0;
+        boolean take = true;
 
-        for(int i = 0; i < nums.length; i = i + 2) {
-            ans += nums[i];
+        for(int i = 0; i < freq.length; i++) {
+            while(freq[i] > 0) {
+                if(take) {
+                    ans += i - 10000;
+                }
+                take = !take;
+                freq[i]--;
+            }
         }
 
         return ans;

eebbe3bf259ab397d1cde4387f534b53cab093ff
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-07-27 23:30:26 +0530

    Time: 46 ms (92.34%), Space: 54.1 MB (93%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -5,8 +5,14 @@
 var compose = function(functions) {
     
     return function(x) {
-        return functions.reduceRight((result, fn) => fn(result), x);
-    }
+        let result = x;
+
+        for(let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result);
+        }
+
+        return result;
+    };
 };
 
 /**

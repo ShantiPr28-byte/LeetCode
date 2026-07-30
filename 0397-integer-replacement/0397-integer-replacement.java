847c06440b079d27f48992b728b6cb502b2e7611
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-07-31 00:12:28 +0530

    Time: 0 ms (100%), Space: 41.9 MB (89.41%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.java
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -4,18 +4,16 @@ class Solution {
         int count = 0;
 
         while(num != 1) {
-            if(num % 2 == 0) {
-                num /= 2;
-                count++;
+            if((num & 1) == 0) {
+                num >>= 1;
             } else {
                 if(num == 3 || ((num & 3) == 1)) {
                     num--;
-                    count++;
                 } else {
                     num++;
-                    count++;
                 }
             }
+            count++;
         }
 
         return count;

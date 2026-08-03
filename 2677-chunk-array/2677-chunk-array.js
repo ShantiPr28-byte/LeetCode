32b95e6e7c8c7979764e495af62836b05fda59a5
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-08-03 16:27:23 +0530

    Time: 36 ms (94.85%), Space: 58.4 MB (5.23%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -7,7 +7,13 @@ var chunk = function(arr, size) {
     let ans = [];
 
     for(let i = 0; i < arr.length; i += size) {
-        ans.push(arr.slice(i, i + size));
+        let temp = [];
+
+        for(let j = i; j < Math.min(arr.length, i + size); j++) {
+            temp.push(arr[j]);
+        }
+
+        ans.push(temp);
     }
 
     return ans;

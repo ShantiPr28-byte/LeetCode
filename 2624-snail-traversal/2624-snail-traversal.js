9a050de7ca24bd46c29caf58d7422c83021c169e
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-08-04 01:08:20 +0530

    Time: 161 ms (49.55%), Space: 68.3 MB (86.35%) - LeetHub

---
+++ 2624-snail-traversal/2624-snail-traversal.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number} rowsCount
+ * @param {number} colsCount
+ * @return {Array<Array<number>>}
+ */
+Array.prototype.snail = function(rowsCount, colsCount) {
+    if(rowsCount * colsCount !== this.length) {
+        return [];
+    }
+
+    const result = Array.from(
+        {length: rowsCount},
+        () => Array(colsCount)
+    );
+
+    index = 0;
+
+    for(let col = 0; col < colsCount; col++) {
+        if(col % 2 === 0) {
+            // up to down
+            for(let row = 0; row < rowsCount; row++) {
+                result[row][col] = this[index++];
+            }
+        } else {
+            // down to up
+            for(let row = rowsCount - 1; row >= 0; row--) {
+                result[row][col] = this[index++];
+            }
+        }
+    }
+
+    return result;
+}
+
+/**
+ * const arr = [1,2,3,4];
+ * arr.snail(1,4); // [[1,2,3,4]]
+ */

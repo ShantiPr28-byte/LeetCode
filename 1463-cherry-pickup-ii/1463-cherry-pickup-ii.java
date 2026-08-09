aa293e61aed5db32a5dc922369af5fd142a4fb6d
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-08-09 22:55:07 +0530

    Time: 25 ms (66.19%), Space: 46.9 MB (20.67%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -5,33 +5,37 @@ class Solution {
 
         Integer[][][] dp = new Integer[n][m][m];
 
-        return Math.max(0, solve(0, 0, m-1, grid, dp));
+        return solve(0, 0, m - 1, grid, dp);
     }
 
-    private int solve(int r1, int c1, int c2, int[][] grid, Integer[][][] dp) {
+    private int solve(int r, int c1, int c2, int[][] grid, Integer[][][]dp) {
         int n = grid.length;
         int m = grid[0].length;
 
-        if(r1 >= n || c1 >= m || c2 >= m || c1 < 0 || c2 < 0) return Integer.MIN_VALUE;
+        if(r >= n || c1 >= m || c2 >= m || c1 < 0 || c2 < 0) return (int)-1e8;
 
-        int cherry = grid[r1][c1];
-
-        if(c1 != c2) cherry += grid[r1][c2];
-
-        if(r1 == n-1) return cherry;
-
-        if(dp[r1][c1][c2] != null) return dp[r1][c1][c2];
-
-        int best = Integer.MIN_VALUE;
-        for(int d1 = -1; d1 <= 1; d1++) {
-            for(int d2 = -1; d2 <= 1; d2++) {
-                int ans = solve(r1+1, c1 + d1, c2 + d2, grid, dp);
-                best = Math.max(best, ans);
+        if(r == n-1) {
+            if(c1 == c2) {
+                return grid[r][c1];
+            } else {
+                return grid[r][c1] + grid[r][c2];
             }
         }
 
-        cherry += best;
+        if(dp[r][c1][c2] != null) return dp[r][c1][c2];
 
-        return dp[r1][c1][c2] = cherry;
+        int max = 0;
+        for(int i = -1; i < 2; i++) {
+            for(int j = -1; j < 2; j++) {
+                if(c1 == c2) {
+                    max = Math.max(max, grid[r][c1] + solve(r + 1, c1 + i, c2 + j, grid, dp));
+                } else {
+                    max = Math.max(max, grid[r][c1] + grid[r][c2] + solve(r + 1, c1 + i, c2 + j, grid, dp));
+                }
+                
+            }
+        }
+
+        return dp[r][c1][c2] = max;
     }
-} 
+}

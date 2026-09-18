6f9bdf91ab2b16bb7460e32a1d36a75a33d002bd
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-09-18 16:42:54 +0530

    Time: 4 ms (16.48%), Space: 45.1 MB (41.97%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,23 +1,30 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        
+        Set<List<Integer>> set = new HashSet<>();
+
+        helper(0, new ArrayList<>(), nums, set);
+
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(0, nums, new ArrayList<>(), result);
-        
+
+        for(List<Integer> list : set) {
+            result.add(list);
+        }
+
         return result;
     }
 
-    private void backtrack(int start, int[] nums, List<Integer> current, List<List<Integer>> result) {
-        result.add(new ArrayList<>(current));
-
-        for (int i = start; i < nums.length; i++) {
-
-            if (i > start && nums[i] == nums[i - 1]) continue;
-
-            current.add(nums[i]);                // choose
-            backtrack(i + 1, nums, current, result); // explore
-            current.remove(current.size() - 1);  // un-choose (backtrack)
+    private void helper(int idx, List<Integer> curr, int[] nums, Set<List<Integer>> set) {
+        if(idx == nums.length) {
+            List<Integer> temp = new ArrayList<>(curr);
+            Collections.sort(temp);
+            set.add(temp);
+            return;
         }
+
+        curr.add(nums[idx]);
+        helper(idx + 1, curr, nums, set);
+
+        curr.remove(curr.size() - 1);
+        helper(idx + 1, curr, nums, set);
     }
 }

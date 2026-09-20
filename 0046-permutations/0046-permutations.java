817b00b15cfe1770fb11b27678554eed3dc20b68
ShantiPr28-byte <shantipriya2812@gmail.com>
2026-09-20 16:46:12 +0530

    Time: 1 ms (87.89%), Space: 45.3 MB (78.82%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,33 +1,30 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        findPermutation(0, nums, result);
+
+        boolean[] taken = new boolean[nums.length];
+
+        helper(0, new ArrayList<>(), taken, nums, result);
 
         return result;
     }
 
-    private void findPermutation(int idx, int[] nums, List<List<Integer>> result) {
+    private void helper(int idx, List<Integer> curr, boolean[] taken, int[] nums, List<List<Integer>> result) {
         if(idx == nums.length) {
-            List<Integer> curr = new ArrayList<>();
-            for(int num : nums) {
-                curr.add(num);
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(!taken[i]) {
+                curr.add(nums[i]);
+                taken[i] = true;
+
+                helper(idx + 1, curr, taken, nums, result);
+
+                curr.remove(curr.size() - 1);
+                taken[i] = false;
             }
-
-            result.add(curr);
         }
-
-        for(int i = idx; i < nums.length; i++) {
-            swap(idx, i, nums);
-
-            findPermutation(idx + 1, nums, result);
-
-            swap(idx, i, nums);
-        }
-    }
-
-    private void swap(int i, int j, int[] nums) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
